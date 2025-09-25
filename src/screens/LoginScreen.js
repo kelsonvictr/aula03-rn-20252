c78671c781c95498717b9072b437c14e778a5b3b
Kelson Almeida <kelson.victr@gmail.com>
2025-09-25 19:48:38 -0300

    feat: style login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
@@ -8,5 +8,12 @@ const LoginScreen = () => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1,  justifyContent: 'center', alignItems: 'center', padding: 20  },
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+  input: { width: '100%', borderBottomWidth: 1, marginBottom: 20, padding: 8},
+  orText: { marginVertical: 20, fontSize: 16, color: '#555' }
+})
 
 export default LoginScreen
